5c05cb3b4ddff846f81e8cdc1bdd8fc7bab6aa8b
ChenChen <cc2286927@sina.cn>
2021-08-18 14:47:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,3 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
-
-# Modify default IP
-sed -i 's/192.168.1.1/4.1.1.1/g' package/base-files/files/bin/config_generate
