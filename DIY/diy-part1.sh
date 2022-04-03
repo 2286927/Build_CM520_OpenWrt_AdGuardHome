7515cb494ab8a2446e321484a0dd59c94f5c5fa0
ChenChen <cc2286927@sina.cn>
2022-04-04 00:22:31 +0800

    Update diy-part1.sh

--- DIY/diy-part1.sh
+++ DIY/diy-part1.sh
@@ -29,7 +29,7 @@ sed -i 's/root::0:0:99999:7:::/root::0:0:99999:7:::/g' package/base-files/files/
 #git clone https://github.com/kenzok8/small.git package/small
 #git clone https://github.com/kiddin9/openwrt-packages.git package/kiddin9
 #git clone https://github.com/NueXini/NueXini_Packages.git package/NueXini
-sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
+sed -i '$a src-git NueXiniPackages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
 
 #Add The Theme：luci-theme-opentomcat
 #git clone https://github.com/Cosme927/luci-theme-opentomcat package/luci-theme-opentomcat
