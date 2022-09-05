c19c7410ed50f650d81f5814ee1b01cd44fa19c9
ChenChen <cc2286927@sina.cn>
2022-09-05 08:38:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,9 +12,11 @@
 # Modify default IP
 sed -i 's/192.168.1.1/4.1.1.1/g' package/base-files/files/bin/config_generate
 # 修改机器名称
-sed -i 's/OpenWrt/CM520-79F/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/CM520-79F-Home/g' package/base-files/files/bin/config_generate
 # 修改机器初始密码
 #sed -i 's/root::0:0:99999:7:::/root::0:0:99999:7:::/g' package/base-files/files/etc/shadow
 # zerotier
 #sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.8.4/g' feeds/packages/net/zerotier/Makefile
 #rm -rf feeds/small/v2ray-core
+# DNS Cache FixUP
+echo -e "#max-ttl=600\nneg-ttl=600\nmin-cache-ttl=3600" >> package/network/services/dnsmasq/files/dnsmasq.conf
