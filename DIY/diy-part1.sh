f5070cd066fcd75fb9e176764a429485fe63dce4
ChenChen <cc2286927@sina.cn>
2022-12-25 10:24:03 +0800

    Update diy-part1.sh

--- DIY/diy-part1.sh
+++ DIY/diy-part1.sh
@@ -34,6 +34,7 @@
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 git clone https://github.com/kenzok8/small-package package/small-package
+git clone https://github.com/imapproxy/luci-app-turboacc package/turboacc
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 #Add The Theme：luci-theme-opentomcat
 #git clone https://github.com/Cosme927/luci-theme-opentomcat package/luci-theme-opentomcat
