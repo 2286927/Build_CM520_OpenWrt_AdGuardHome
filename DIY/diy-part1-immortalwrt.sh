248485fe5317b2951bb53fe68f5ecfcb9f931e6a
ChenChen <cc2286927@sina.cn>
2023-01-25 09:46:12 +0800

    Update diy-part1-immortalwrt.sh

--- DIY/diy-part1-immortalwrt.sh
+++ DIY/diy-part1-immortalwrt.sh
@@ -39,7 +39,7 @@ git clone https://github.com/kenzok8/small-package package/small-package
 #git clone https://github.com/imapproxy/luci-app-turboacc.git package/small-package/luci-app-turboacc
 svn export --force https://github.com/kiddin9/openwrt-packages/trunk/luci-app-turboacc package/small-package/luci-app-turboacc
 svn export --force https://github.com/kiddin9/openwrt-packages/trunk/luci-app-rclone package/small-package/luci-app-rclone
-git clone https://github.com/bootli/luci-app-ddns-go.git package/ddns-g
+git clone https://github.com/bootli/luci-app-ddns-go.git package/ddns-go
 #git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 #Add The Theme：luci-theme-opentomcat
 #git clone https://github.com/Cosme927/luci-theme-opentomcat package/luci-theme-opentomcat
