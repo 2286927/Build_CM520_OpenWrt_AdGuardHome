643593a3a4f7aae69f2254cc733c5d26e289a20d
ChenChen <cc2286927@sina.cn>
2023-03-19 08:48:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -39,10 +39,10 @@ svn export --force https://github.com/kiddin9/openwrt-packages/trunk/luci-app-tu
 svn export --force https://github.com/kiddin9/openwrt-packages/trunk/shortcut-fe package/small-package/luci-app-turboacc/shortcut-fe
 
 ## DDNS-Go
-#svn export --force https://github.com/kiddin9/openwrt-packages/trunk/ddnsgo package/kiddin-package/ddnsgo
-#svn export --force https://github.com/kiddin9/openwrt-packages/trunk/luci-app-ddns-go package/kiddin-package/luci-app-ddns-go
-svn export --force https://github.com/immortalwrt/luci/trunk/applications/luci-app-ddns-go package/immortalwrt/luci-app-ddns-go
-svn export --force https://github.com/immortalwrt/packages/trunk/net/ddns-go package/immortalwrt/luci-app-ddns-go
+svn export --force https://github.com/kiddin9/openwrt-packages/trunk/ddnsgo package/kiddin-package/ddnsgo
+svn export --force https://github.com/kiddin9/openwrt-packages/trunk/luci-app-ddns-go package/kiddin-package/luci-app-ddns-go
+#svn export --force https://github.com/immortalwrt/luci/trunk/applications/luci-app-ddns-go package/immortalwrt/luci-app-ddns-go
+#svn export --force https://github.com/immortalwrt/packages/trunk/net/ddns-go package/immortalwrt/luci-app-ddns-go
 #
 
 svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-rclone package/small-package/luci-app-rclone
