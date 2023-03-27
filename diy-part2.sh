3847ccb07a7cea46798a740dffff3ab07d13f2e6
ChenChen <cc2286927@sina.cn>
2023-03-27 15:38:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,7 +24,7 @@ sed -i 's/ImmortalWrt/CM520-79F-Home/g' package/base-files/files/bin/config_gene
 sed -i 's/ImmortalWrt/CM520-79F-Home/g' package/base-files/files/bin/config_generate
 ####### Modify the version number
 sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_DESCRIPTION='ImmortalWrt $('+%V')'" >> package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='ImmortalWrt +%V'" >> package/base-files/files/etc/openwrt_release
 sed -i "s/ImmortalWrt /洲·Cy build $(TZ=UTC-8 date "+%Y.%m.%d") @ ImmortalWrt /g" package/base-files/files/etc/openwrt_release
 # DNS Cache FixUP
 echo -e "#max-ttl=600\nneg-ttl=600\nmin-cache-ttl=3600" >> package/network/services/dnsmasq/files/dnsmasq.conf
