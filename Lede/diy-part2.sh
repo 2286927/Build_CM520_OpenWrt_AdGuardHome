2a1912601ad49cb1d38b4f077cf3cf0ca453ab9c
ChenChen <cc2286927@sina.cn>
2023-05-13 21:56:25 +0800

    Update diy-part2.sh

--- Lede/diy-part2.sh
+++ Lede/diy-part2.sh
@@ -29,6 +29,7 @@ sed -i 's/OpenWrt/CM520-79F-Home/g' package/base-files/files/bin/config_generate
 sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
 echo "DISTRIB_DESCRIPTION='OpenWrt $('%V')'" >> package/base-files/files/etc/openwrt_release
 sed -i "s/OpenWrt /\洲\·\C\y \build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/base-files/files/etc/openwrt_release
+#
 sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/arm/index.htm
 
 sed -i '741a\
